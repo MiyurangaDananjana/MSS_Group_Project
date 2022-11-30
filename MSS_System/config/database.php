163d468d057e96851d376a1fa6456fc9c2087bd3
MiyurangaDananjana <83136587+MiyurangaDananjana@users.noreply.github.com>
2022-12-01 02:11:22 +0530

    Re_Install_Laravel
    Re_Install_Laravel add database connection and laravel breeze auth

--- MSS_System/config/database.php
+++ MSS_System/config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '198.37.120.210'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'aperatel_MSS'),
+            'username' => env('DB_USERNAME', 'aperatel_MSS'),
+            'password' => env('DB_PASSWORD', 'E4gW6EJl4&O*'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -58,9 +58,11 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql')
+                ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                ])
+                : [],
         ],
 
         'pgsql' => [
